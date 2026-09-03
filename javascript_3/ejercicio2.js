97bd5858b375ab89de56d887e56fa22eca82d91f
Jose Muñoz <joseandres.munoz@gmail.com>
2026-09-03 15:53:12 -0300

    Add files via upload
    Ejercicio modulo 3 unidad 4

---
+++ javascript_3/ejercicio2.js
@@ -0,0 +1,10 @@
+const producto = {
+id: 123,
+nombre: "Auriculares inalámbricos",
+precio: 120000,
+stock: 25,
+categoria: "Electrónica"
+};
+
+const {nombre, precio, stock} = producto;
+console.log(producto);
